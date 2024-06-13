a85ed3f28633cdceb8a8da37152a656063bda6a9
aledx18 <alediiaz18@gmail.com>
2024-06-13 12:48:24 -0300

    logs:2

--- middleware.ts
+++ middleware.ts
@@ -1,8 +1,19 @@
 import { NextResponse } from 'next/server'
+import type { NextRequest } from 'next/server'
 
-export function middleware(request: any) {
+export function middleware(request: NextRequest) {
   const response = NextResponse.next()
-  response.headers.set('x-latitude', request.geo.latitude)
-  response.headers.set('x-longitude', request.geo.longitude)
+
+  const { geo, url, nextUrl, redirect } = request
+
+  console.log('url', url)
+  console.log('nextUrl', nextUrl)
+  console.log('redirect', redirect)
+  const latitude = geo?.latitude || 'unknown'
+  const longitude = geo?.longitude || 'unknown'
+
+  response.headers.set('x-latitude', latitude)
+  response.headers.set('x-longitude', longitude)
+
   return response
 }
